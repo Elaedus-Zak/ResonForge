f94e33331514715e5ec0e9555c5982fba3851de4
Elaedus-Zak <zakaria777adlouni@gmail.com>
2026-04-09 01:12:48 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   
-  baseURL: 'http://192.168.100.39:8080', 
+  baseURL: 'https://resonforge-api-production.up.railway.app:8080', 
 
   
 });
